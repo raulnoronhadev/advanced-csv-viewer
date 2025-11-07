c78df316fecea200c6913e3fa65aca6a2c0161a6
raul <raulnoronhadev@gmail.com>
2025-11-07 00:02:55 -0300

    Deploy configs

--- src/context/CsvDataContext.tsx
+++ src/context/CsvDataContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, type ReactNode, type ChangeEvent } from "react";
+import { createContext, useContext, useState, type ReactNode } from "react";
 import * as Papa from 'papaparse';
 import { type ColDef } from 'ag-grid-community';
 
--- src/pages/View/index.tsx
+++ src/pages/View/index.tsx
@@ -1,10 +1,7 @@
-import { Box, Container, useTheme } from '@mui/material';
-import { tokens } from "../../context/ThemeContext";
+import { Container } from '@mui/material';
 import CsvGrid from './CsvGrid';
 
 export default function View() {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
 
     return (
         <Container maxWidth="lg">
